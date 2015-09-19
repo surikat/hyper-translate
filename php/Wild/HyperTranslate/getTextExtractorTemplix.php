979c93967f7e86d7c9ef0da11019bb00e64e5ba5
surikat <jo@surikat.pro>
2015-09-19 22:14:45 +0200

    add href extraction

--- php/Wild/HyperTranslate/getTextExtractorTemplix.php
+++ php/Wild/HyperTranslate/getTextExtractorTemplix.php
@@ -27,7 +27,7 @@ class getTextExtractorTemplix extends getTextExtractor{
 		$TML('*')->each(function($el)use(&$msg,$filename){
 			foreach($el->attributes as $k=>$v){
 				$v = trim($v);
-				if($v&&(($k=='title'&&strpos($v,'<?')===false)||strpos($k,'i18n-')))
+				if($v&&(($k='href'&&$el->nodeName=='a')||($k=='title'&&strpos($v,'<?')===false)||strpos($k,'i18n-')))
 					$msg .= "#: $filename \nmsgid ".self::quote($v)."\nmsgstr \"\" \n\n";					
 			}
 		});
