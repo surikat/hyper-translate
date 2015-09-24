ff53ffc9aede5c649003d545f3b0097cf70b43e0
surikat <jo@surikat.pro>
2015-09-24 18:24:41 +0200

    typo bugfix

--- php/Wild/HyperTranslate/getTextExtractorTemplix.php
+++ php/Wild/HyperTranslate/getTextExtractorTemplix.php
@@ -29,9 +29,9 @@ class getTextExtractorTemplix extends getTextExtractor{
 				$v = trim($v);
 				if($v&&
 					(
-						($k='href'&&$el->nodeName=='a'&&strpos($v,'://')!==false&&strpos($v,'<?')===false)
-						||($k='alt'&&$el->nodeName=='img'&&strpos($v,'<?')===false)
-						||($k='title'&&strpos($v,'<?')===false)
+						($k=='href'&&$el->nodeName=='a'&&strpos($v,'://')!==false&&strpos($v,'<?')===false)
+						||($k=='alt'&&$el->nodeName=='img'&&strpos($v,'<?')===false)
+						||($k=='title'&&strpos($v,'<?')===false)
 						||($k=='title'&&strpos($v,'<?')===false)
 						||strpos($k,'i18n-')
 					)
